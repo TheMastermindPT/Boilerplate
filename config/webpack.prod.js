cba22a588518d3e584053cd641d512f7a9098db9
Mastermind <38897046+TheMastermindPT@users.noreply.github.com>
2018-10-17 18:44:55 +0100

    new boiler

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: ["./src/client/main.js"]
+    main: ['./src/client/main.js'],
   },
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "scripts/[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
+    filename: 'scripts/[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -20,59 +20,81 @@ module.exports = {
         test: /\.js$/,
         use: [
           {
-            loader: "babel-loader"
-          }
+            loader: 'babel-loader',
+          },
         ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?[ac]ss$/,
         use: [
           {
-            loader: MiniCSSExtractPlugin.loader
+            loader: MiniCSSExtractPlugin.loader,
           },
           {
-            loader: "css-loader"
+            loader: 'css-loader',
           },
           {
-            loader: "postcss-loader"
+            loader: 'postcss-loader',
           },
           {
-            loader: "sass-loader"
-          }
-        ]
+            loader: 'sass-loader',
+          },
+        ],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: {
-              attrs: ["img:src"]
-            }
-          }
-        ]
+              attrs: ['img:src'],
+            },
+          },
+        ],
       },
       {
-        test: /\.(jpg|png|gif|svg)$/,
+        test: /\.(jpg|png|gif|jpeg|.ico)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "imgs/[name].[ext]"
-            }
-          }
-        ]
-      }
-    ]
+              name: '[name].[ext]',
+              outputPath: 'imgs/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'svg/',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new OptimizeCssAssetsPlugin(),
     new MiniCSSExtractPlugin({
-      filename: "styles/[name].css"
+      filename: 'styles/[name].css',
     }),
     new HTMLWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+      },
+    }),
+  ],
+  externals: {
+    jquery: 'jQuery',
+  },
 };
